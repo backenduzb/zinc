6b9056e6d4e66027a1a1d0f7d3b862372ccfda49
Javohir <qobiljonsharipov33@gmail.com>
2026-02-24 13:33:35 +0500

    Removed cache files and readded files for ignore other files

--- include/zinc/string/string.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <stdint.h>
-#include <stddef.h>
-
-size_t utoa(uint64_t value, char* buffer);
-size_t count(const char *str, const char sep);
-size_t len(const char *str);
-int strcmp(const char *a, const char *b);
