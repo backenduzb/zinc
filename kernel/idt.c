dd3c12a0a03314d4dc2501b0b191b7732149b26c
Javohir <qobiljonsharipov33@gmail.com>
2026-03-17 19:41:02 +0500

    Tested on real time

---
+++ kernel/idt.c
@@ -0,0 +1,39 @@
+#include <idt.h>
+#include <stdint.h>
+
+extern void idt_load(struct idt_ptr *idtr);
+extern void isr_timer(void);
+
+static struct idt_entry idt[256];
+
+static void idt_set_gate(uint8_t vector, void *isr) {
+    uint64_t addr = (uint64_t)isr;
+
+    idt[vector].offset_low = (uint16_t)(addr & 0xFFFF);
+    idt[vector].selector = 0x08;
+    idt[vector].ist = 0;
+    idt[vector].type_attr = 0x8E;
+    idt[vector].offset_mid = (uint16_t)((addr >> 16) & 0xFFFF);
+    idt[vector].offset_high = (uint32_t)((addr >> 32) & 0xFFFFFFFF);
+    idt[vector].zero = 0;
+}
+
+void idt_init(void) {
+    for (int i = 0; i < 256; i++) {
+        idt[i].offset_low = 0;
+        idt[i].selector = 0;
+        idt[i].ist = 0;
+        idt[i].type_attr = 0;
+        idt[i].offset_mid = 0;
+        idt[i].offset_high = 0;
+        idt[i].zero = 0;
+    }
+
+    idt_set_gate(32, isr_timer);
+
+    struct idt_ptr idtr;
+    idtr.limit = (uint16_t)(sizeof(idt) - 1);
+    idtr.base = (uint64_t)&idt[0];
+
+    idt_load(&idtr);
+}
--- kernel/main.c
+++ kernel/main.c
@@ -1,9 +1,31 @@
 #include <stdint.h>
 #include <fb/fb.h>
+#include <idt.h>
+#include <pic.h>
+#include <timer/pit.h>
 
 void kernel_main(uint64_t magic, uint64_t mbi_addr) {
-    init_framebuffer(mbi_addr);
-    draw_line();
+    if (magic != MULTIBOOT2_BOOTLOADER_MAGIC) {
+        while (1) { }
+    }
 
-    while (1) { }
+    idt_init();
+    pic_remap(0x20, 0x28);
+    pic_set_mask(0, 0);
+    pit_init(1000);
+    __asm__ volatile("sti");
+
+    init_framebuffer(mbi_addr);
+
+    //sleep(3000);
+    //draw_line();
+    //sleep(5000);
+    //draw_line_white();
+
+    while (1) {
+        sleep(3000);
+        draw_line();
+        sleep(3000);
+        draw_line_white();
+    }
 }
