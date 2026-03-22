1ffdfb894d7afa302a770af9178d942970218e41
Javohir <qobiljonsharipov33@gmail.com>
2026-03-22 21:13:11 +0500

    At the time waybar clock refreshing clear

--- ui/components/waybar.c
+++ ui/components/waybar.c
@@ -5,6 +5,10 @@
 #include <ui/label.h>
 
 void draw_waybar() {
-    fill_rect(0, 0, 0, 30, 0x003b3b3b);
-    center_label(15, "15:00:00", 0x00FFFFFF);
+    fill_rect(0, 0, 0, 30, 0x001b1b1b);
+    char time_bufer[9];
+    get_time(time_bufer);
+    center_label(15, time_bufer, 0x00FFFFFF);
+    sleep(100);
+    center_label(15, time_bufer, 0x001b1b1b);
 }
