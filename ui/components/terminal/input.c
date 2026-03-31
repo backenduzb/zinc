098ad6a7318971d4802aec0f2c529626ff0af3a5
Javohir <qobiljonsharipov33@gmail.com>
2026-03-31 21:13:42 +0500

    Writed backspace button

--- ui/components/terminal/input.c
+++ ui/components/terminal/input.c
@@ -1,7 +1,16 @@
 #include <fb/fb.h>
 #include <font/psf1.h>
+#include <stdint.h>
 
 #include "terminal.h"
+
+static void clear_char(uint32_t row, uint32_t col) {
+    for (uint32_t r = 0; r < 16; r++) {
+        for (uint32_t c = 0; c < 8; c++) {
+            framebuffer[(row + r) * width + (col + c)] = 0x000000;
+        }
+    }
+}
 
 static void terminal_wrap_if_needed(uint32_t glyph_h) {
     if (terminal_state.col + GLYPH_WIDTH > (width - TERMINAL_PADDING_X)) {
@@ -32,7 +41,6 @@ void termwrite(char ch, uint32_t color) {
     }
 
     glyph_h = psf1_get_height();
-    cursor_update(terminal_state.row + TERMINAL_CURSOR_OFFSET_Y, terminal_state.col + GLYPH_WIDTH, 0x00FFFFFF);
 
     if (ch == '\n') {
         terminal_state.input_buffer[terminal_state.input_length] = '\0';
@@ -45,11 +53,32 @@ void termwrite(char ch, uint32_t color) {
         terminal_state.col = terminal_write_prompt(terminal_state.col, terminal_state.row, color);
         return;
     }
+    if (ch == '\b') {
+        if (terminal_state.input_length > 0) {
+            terminal_state.input_length--;
+            terminal_state.input_buffer[terminal_state.input_length] = '\0';
+    
+            terminal_state.col -= GLYPH_WIDTH;
+            clear_char(terminal_state.row, terminal_state.col);
+        }
+    
+        cursor_update(
+            terminal_state.row + TERMINAL_CURSOR_OFFSET_Y,
+            terminal_state.col,
+            0x00FFFFFF
+        );
+        return;
+    }
 
     if (terminal_state.input_length + 1u >= TERMINAL_INPUT_BUFFER_SIZE) {
         return;
     }
 
+    cursor_update(
+        terminal_state.row + TERMINAL_CURSOR_OFFSET_Y,
+        terminal_state.col + GLYPH_WIDTH,
+        0x00FFFFFF
+    );
     terminal_wrap_if_needed(glyph_h);
     terminal_state.input_buffer[terminal_state.input_length++] = ch;
     terminal_state.input_buffer[terminal_state.input_length] = '\0';
