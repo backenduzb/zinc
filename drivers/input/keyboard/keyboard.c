2585e769bef484d0f75d7fc26631b4c18c5d766a
Javohir <qobiljonsharipov33@gmail.com>
2026-03-31 20:10:50 +0500

    added capslock button

--- drivers/input/keyboard/keyboard.c
+++ drivers/input/keyboard/keyboard.c
@@ -7,9 +7,10 @@
 #define KBD_DATA_PORT 0x60
 
 static volatile uint8_t shift_down = 0;
+static volatile uint8_t caps_down = 0;
 
 static const char scancode_map[128] = {
-    0,  27, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 0,  0,
+    0,  27, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '\b',  0,
     'q','w','e','r','t','y','u','i','o','p','[',']','\n',0,  'a','s',
     'd','f','g','h','j','k','l',';','\'','`',0, '\\','z','x','c','v',
     'b','n','m',',','.','/',0,  '*',0,  ' ',0,  0,  0,  0,  0,  0,
@@ -19,7 +20,7 @@ static const char scancode_map[128] = {
 };
 
 static const char scancode_shift_map[128] = {
-    0,  27, '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 0,  0,
+    0,  27, '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '\b',  0,
     'Q','W','E','R','T','Y','U','I','O','P','{','}','\n',0,  'A','S',
     'D','F','G','H','J','K','L',':','"','~',0,  '|','Z','X','C','V',
     'B','N','M','<','>','?',0,  '*',0,  ' ',0,  0,  0,  0,  0,  0,
@@ -32,7 +33,13 @@ static char scancode_to_ascii(uint8_t scancode) {
     if (scancode >= 128) {
         return 0;
     }
-    return shift_down ? scancode_shift_map[scancode] : scancode_map[scancode];
+    char ch;
+    if (shift_down) ch = scancode_shift_map[scancode];
+    else ch = scancode_map[scancode];
+
+    if (caps_down && ch >= 'a' && ch <= 'z') ch -= 32;
+    else if (caps_down && ch >= 'A' && ch <= 'Z') ch +=32;
+    return ch;
 }
 
 void keyboard_init(void) {
@@ -48,16 +55,21 @@ void keyboard_handler(void) {
     }
 
     if (scancode & 0x80) {
-        uint8_t key = (uint8_t)(scancode & 0x7F);
+        uint8_t key = scancode & 0x7F;
         if (key == 0x2A || key == 0x36) {
             shift_down = 0;
         }
         pic_send_eoi(1);
         return;
     }
-
     if (scancode == 0x2A || scancode == 0x36) {
         shift_down = 1;
+        pic_send_eoi(1);
+        return;
+    }
+    
+    if (scancode == 0x3A) {
+        caps_down = !caps_down;
         pic_send_eoi(1);
         return;
     }
